26bb5430c8635b53d4e024e8d2cc8877b565bc82
sasi143 <s.sasisekhar143@gmail.com>
2019-04-10 00:47:47 +0530

    sdfghjksdfghjkl

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "App running successfully ssekhar"
+  return "App running successfully sadam"
   
 @app.route('/test')
 def index1():
-  return "How are you doing buddy"
+  return "How are you doing aditya"
 
   
 if __name__ == "__main__":
