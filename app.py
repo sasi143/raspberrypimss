1456d2a99a894bcbad045f013209bc79a1aa07f7
sasi143 <s.sasisekhar143@gmail.com>
2019-04-10 00:43:06 +0530

    sdfghjk

--- app.py
+++ app.py
@@ -4,8 +4,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return "App running successfully sasi"
+  return "App running successfully ssekhar"
   
+@app.route('/test')
+def index1():
+  return "How are you doing buddy"
+
   
 if __name__ == "__main__":
   app.run(host="0.0.0.0",port="5000")
